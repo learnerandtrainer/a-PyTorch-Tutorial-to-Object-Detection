187150d9cf4657b05ab10edd1d106c44812e37a3
learnerandtrainer <66632022+learnerandtrainer@users.noreply.github.com>
2020-07-01 17:25:28 +0300

    Update create_data_lists.py
    changed depend on utils.py's changes and folde structure

--- create_data_lists.py
+++ create_data_lists.py
@@ -1,6 +1,5 @@
 from utils import create_data_lists
 
 if __name__ == '__main__':
-    create_data_lists(voc07_path='/media/ssd/ssd data/VOC2007',
-                      voc12_path='/media/ssd/ssd data/VOC2012',
-                      output_folder='./')
+    create_data_lists(data_folder='/content/train',
+                      output_folder='/content/train/')
